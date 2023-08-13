d6e13b50abadfc5eac230e2cdac30bad268bfb79
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2023-08-13 20:28:20 +0530

    Update grep.c

--- grep.c
+++ grep.c
@@ -1,19 +1,30 @@
-#include<stdio.h>
-#include<dirent.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+
 int main() {
-    char fn[10], pat[10], temp[200];
+    char fn[100], pat[100], temp[200];
     FILE *fp;
-    printf("\n Enter file name : ");
+
+    printf("Enter file name: ");
     scanf("%s", fn);
+
     printf("Enter the pattern: ");
     scanf("%s", pat);
+
     fp = fopen(fn, "r");
-    while (!feof(fp)) {
-        fgets(temp, sizeof(fp), fp);
-        if (strcmp(temp, pat))
-            printf("%s", temp);
+
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return 1;
     }
+
+    while (fgets(temp, sizeof(temp), fp) != NULL) {
+        if (strstr(temp, pat) != NULL) {
+            printf("%s", temp);
+        }
+    }
+
     fclose(fp);
-    return 1;
+
+    return 0;
 }
