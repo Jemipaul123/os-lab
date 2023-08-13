d5b12796a46e2cb2cc60442106a9a56b23e431d3
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2023-08-13 21:44:41 +0530

    Update worstfit.c

--- worstfit.c
+++ worstfit.c
@@ -1,63 +1,66 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAX_MEMORY 1024 // maximum memory size
-int memory[MAX_MEMORY]; // memory array
-int num_processes = 0; // number of processes
+int main() {
+    int i, j, blocknos, blocksize[10], processnos, processsize[10];
+    int t, flag, largestsize, largestindex;
 
-// allocate memory using worst fit algorithm
-int worstFit(int process_size)
-{
-    int worst_fit = -1, worst_index = -1;
-    for(int i=0; i<MAX_MEMORY; i++)
-    {
-        if(memory[i] == 0)
-        {
-            int j = i;
-            while(memory[j] == 0 && j < MAX_MEMORY && j-i+1 < process_size)
-                j++;
-            if(j-i+1 >= process_size && j-i+1 > worst_fit)
-            {
-                worst_fit = j-i+1;
-                worst_index = i;
+    printf("Enter the no of memory blocks: ");
+    scanf("%d", &blocknos);
+
+    printf("Enter the size of each block in order:\n");
+    for (i = 0; i < blocknos; i++) {
+        scanf("%d", &blocksize[i]);
+    }
+
+    printf("Enter the no of processes: ");
+    scanf("%d", &processnos);
+
+    printf("Enter the size of each process:\n");
+    for (i = 0; i < processnos; i++) {
+        scanf("%d", &processsize[i]);
+    }
+
+    printf("Blockno.\tBlocksize\n");
+    for (i = 0; i < blocknos; i++) {
+        printf("%d\t\t%d\n", i + 1, blocksize[i]);
+    }
+
+    printf("Processno.\tProcessSize\n");
+    for (i = 0; i < processnos; i++) {
+        printf("%d\t\t%d\n", i + 1, processsize[i]);
+    }
+
+    printf("WORST FIT MEMORY ALLOCATION\n");
+    printf("Processno\tAllocated blockno\tAllocated blocksize\tFragment left\n");
+
+    i = 0;
+    while (i < processnos) {
+        flag = 0;  // Reset the flag for each process
+        largestsize = -1;  // Initialize the largestsize to an invalid value
+
+        for (j = 0; j < blocknos; j++) {
+            if (processsize[i] <= blocksize[j]) {
+                if (flag == 0) {
+                    largestsize = blocksize[j];
+                    largestindex = j;
+                    flag++;
+                } else if (blocksize[j] > largestsize) {
+                    largestsize = blocksize[j];
+                    largestindex = j;
+                }
             }
-            i = j;
         }
-    }
-    if(worst_index != -1)
-    {
-        for(int k=worst_index; k<worst_index+process_size; k++)
-            memory[k] = num_processes+1;
-        return worst_index;
-    }
-    return -1; // memory allocation failed
-}
 
-// main function
-int main()
-{
-    int choice, process_size, process_id;
-    printf("1. Allocate Memory using Worst Fit\n2. Exit\n");
-    while(1)
-    {
-        printf("\nEnter your choice: ");
-        scanf("%d", &choice);
-        switch(choice)
-        {
-            case 1: printf("\nEnter process size: ");
-                    scanf("%d", &process_size);
-                    process_id = worstFit(process_size);
-                    if(process_id == -1)
-                        printf("\nMemory allocation failed!\n");
-                    else
-                    {
-                        num_processes++;
-                        printf("\nMemory allocated successfully to process %d\n", process_id+1);
-                    }
-                    break;
-            case 2: exit(0);
-            default: printf("\nInvalid choice!\n");
+        if (largestsize != -1) {
+            t = blocksize[largestindex];
+            blocksize[largestindex] = blocksize[largestindex] - processsize[i];
+            printf("%d\t\t\t%d\t\t\t%d\t\t\t%d\n", i + 1, largestindex + 1, t, blocksize[largestindex]);
+        } else {
+            printf("%d\t\t\tNot allocated\n", i + 1);
         }
+
+        i++;
     }
+
     return 0;
 }
